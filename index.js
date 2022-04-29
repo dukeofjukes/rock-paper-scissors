3460b1917685ee5d1feef4f21ebe5e00256aca28
Brandon Burtchell <brandonburtchell@gmail.com>
2022-04-28 21:01:22 -0500

    Create Hand class (currently broken?) and write win check logic

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const playerPoints = 0;
 const computerPoints = 0;
 
+class Hand {
+  static Rock = new Hand("rock");
+  static Paper = new Hand("paper");
+  static Scissors = new Hand("scissors");
+
+  constructor(name) {
+    this.name = name;
+  }
+}
+
 game();
 
 // min included, max excluded
@@ -8,34 +18,72 @@ function randomInt(min, max) {
   return Math.floor(Math.random * (max - min)) + min;
 }
 
-function computerPlay() {
+function getComputerHand() {
   let choice = randomInt(0, 3);
-  if (choice === 0) {
-    return "Rock";
-  } else if (choice === 2) {
-    return "Paper";
-  } else {
-    return "Scissors";
+  let hand;
+  switch (choice) {
+    case 0:
+      hand = Hand.Rock;
+      break;
+    case 1:
+      hand = Hand.Paper;
+      break;
+    default:
+      hand = Hand.Scissors;
+      break;
   }
+
+  console.log("computer played: " + hand);
+  return hand;
+}
+
+function getPlayerHand() {
+  // TODO: replace this primitive user prompt with gui
+  let choice = prompt("Type in 'Rock', 'Paper', or 'Scissors'.").toLowerCase;
+  let hand;
+  switch (choice) {
+    case "rock":
+      hand = Hand.Rock;
+      break;
+    case "paper":
+      hand = Hand.Paper;
+      break;
+    case "scissors":
+      hand = Hand.Scissors;
+      break;
+  }
+
+  console.log("player played: " + hand);
+  return hand;
 }
 
 function playRound() {
-  // TODO: get player choice in a better way
-  let playerChoice = prompt("Type in 'Rock', 'Paper', or 'Scissors'.");
+  let player = getPlayerHand();
+  let computer = getComputerHand();
 
-  let computerChoice = computerPlay();
-
-  let winner = "player";
-  if (playerChoice === "Rock" && computerChoice === "Scissors") {
-    winner = "player";
+  let winner = "PLAYER";
+  if (
+    (player === Hand.Rock && computer === Hand.Scissors) ||
+    (player === Hand.Scissors && computer === Hand.Paper) ||
+    (player === Hand.Paper && computer === Hand.Rock)
+  ) {
+    winner = "PLAYER";
+    playerPoints++;
+  } else if (player === computer) {
+    winner = "DRAW";
+  } else {
+    winner == "COMPUTER";
+    computerPoints;
   }
-  // TODO: finish rock-paper-scissors comparison logic
 
-  console.log(winner);
+  console.log(winner + " won the round.\n");
+  return winner;
 }
 
 function game() {
   while (playerPoints < 5 && computerPoints < 5) {
-    playRound();
+    let winner = playRound();
   }
+
+  console.log(winner + " won the game.\n");
 }
